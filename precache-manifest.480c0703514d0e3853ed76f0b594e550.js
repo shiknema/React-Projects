28232401b9a751276dd5398a20963f3d61d4880c
Shikha Gupta <shiknema@gmail.com>
2021-01-27 00:14:27 +0100

    Updates

---
+++ precache-manifest.480c0703514d0e3853ed76f0b594e550.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ccda8947b9cac83bfb7ab2f12ddea97",
+    "url": "/React-Projects/index.html"
+  },
+  {
+    "revision": "e6d36d1b063ad865cfa1",
+    "url": "/React-Projects/static/css/main.0f17a4bd.chunk.css"
+  },
+  {
+    "revision": "37d693c0db66880c19a2",
+    "url": "/React-Projects/static/js/2.b1a81836.chunk.js"
+  },
+  {
+    "revision": "e6d36d1b063ad865cfa1",
+    "url": "/React-Projects/static/js/main.7c9264b3.chunk.js"
+  },
+  {
+    "revision": "d9111302033859240725",
+    "url": "/React-Projects/static/js/runtime~main.e756cf0a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-Projects/static/media/logo.5d5d9eef.svg"
+  }
+]);
